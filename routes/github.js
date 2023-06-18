12c70cbb1a7b0d8bc604ad5f3c79e1f6be627694
Kael Haney <kaelhaney@gmail.com>
2023-06-18 15:17:54 -0400

    GitHub Webhook Test 3

--- routes/github.js
+++ routes/github.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const crypto = require('crypto');
 
-const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET;
+const GITHUB_WEBHOOK_SECRET = process.env.GITHUB_WEBHOOK_SECRET;
 
 const verifySignature = (req) => {
   const signature = crypto
-    .createHmac("sha256", WEBHOOK_SECRET)
+    .createHmac("sha256", GITHUB_WEBHOOK_SECRET)
     .update(JSON.stringify(req.body))
     .digest("hex");
   return `sha256=${signature}` === req.headers.get("x-hub-signature-256");
