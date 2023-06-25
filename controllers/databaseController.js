61df571861fe1fb6a88d82144c268612af45058b
Kael Haney <kaelhaney@gmail.com>
2023-06-24 21:18:03 -0400

    Fix dbConfig file path

--- controllers/databaseController.js
+++ controllers/databaseController.js
@@ -1,7 +1,7 @@
 const mariadb = require('mariadb');
 const fs = require('fs');
 
-const dbConfig = JSON.parse(fs.readFileSync('../config/dbConfig.json'));
+const dbConfig = JSON.parse(fs.readFileSync('./config/dbConfig.json'));
 
 const dbConnPool = mariadb.createPool(dbConfig);
 
