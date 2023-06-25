bc059af2213ad06406b9aa1c93c83aad83238086
Kael Haney <kaelhaney@gmail.com>
2023-06-24 21:16:08 -0400

    Database Connection Test 3

--- routes/index.js
+++ routes/index.js
@@ -3,16 +3,14 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', async function(req, res, next) {
-  await require('../controllers/databaseController.js').ping()
-    .then(() => {
-      res.locals.dbStatus = 'Connected';
-      res.render('index');
-  	})
-  	.catch(err => {
-      res.locals.dbStatus = 'Disconnected';
-      res.locals.error = err;
-      res.render('index');
-  	});
+  try {
+    await require('../controllers/databaseController.js').ping()
+    res.locals.dbStatus = 'Connected';
+  } catch (err) {
+    res.locals.dbStatus = 'Disconnected';
+    res.locals.error = err;
+  }
+  res.render('index');
 });
 
 module.exports = router;
