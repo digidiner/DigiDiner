f9c21d161018bcc67dd8780590b874c0a5aa94de
Lindsey Jimenez <lindseyjimenez@gmail.com>
2023-06-28 00:00:53 -0400

    Menu test 2

--- routes/menu.js
+++ routes/menu.js
@@ -1,15 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function (req, res) {
-    res.status(200).render('menu');
-});
-
 const menuItems = [
     { name: 'Item 1', description: 'Description 1', price: 9.99, image: '#' },
     { name: 'Item 2', description: 'Description 2', price: 8.99, image: '#' },
     // Add more menu items as needed
 ];
+
+/* GET users listing. */
+router.get('/', function (req, res) {
+    res.status(200).render('menu', { menuItems });
+});
 
 module.exports = router;
