a14e6c8110323ee72b525416f57965fd390a1f98
Kael Haney <kaelhaney@gmail.com>
2023-06-28 20:31:14 -0400

    Hopefuly fix REST API endpoint

--- routes/api/management.js
+++ routes/api/management.js
@@ -1,14 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
+var utils = require('../../utils');
 var employee = require('../../models/employee');
 
-/* GET users listing. */
-router.get('/employee/list', async function(req, res) {
+/* GET employee list */
+router.get('/employee/list', utils.asyncHandler(async function(req, res) {
     let employeeList = await employee.listEmployees();
     res.status(200).json({
         'response': employeeList
     });
-});
+}));
 
 module.exports = router;
