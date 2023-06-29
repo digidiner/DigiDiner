845845b76b89715ee7e6032e01a472f0278c7aef
Kael Haney <kaelhaney@gmail.com>
2023-06-28 20:46:05 -0400

    Use map instead of flat

--- models/employee.js
+++ models/employee.js
@@ -28,7 +28,7 @@ class Employee {
     }
 
     static async listEmployees() {
-        return await Employee.conn.query({ sql: `SELECT id FROM employee`, rowsAsArray: true }).flat();
+        return await Employee.conn.query(`SELECT id FROM employee`).map(record => record.id);
     }
 
     async load() {
