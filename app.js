60830472138adabb7c191ec0bcf0c14e13e534da
Lindsey Jimenez <lindseyjimenez@gmail.com>
2023-06-30 00:29:17 -0400

    Clock test 4. Implemented jquery.

--- app.js
+++ app.js
@@ -15,22 +15,23 @@ var app = express();
 
 async function main() {
   var conn = await require('./controllers/databaseController.js').getConnection();
-  
+
   // view engine setup
   app.set('views', path.join(__dirname, 'views'));
   app.set('view engine', 'ejs');
-  
+
   app.use(logger('dev'));
   app.use(express.json());
   app.use(express.urlencoded({ extended: false }));
   app.use(cookieParser());
+  app.use('/jquery', express.static(path.join(__dirname, 'node_modules/jquery/dist')));
   app.use(express.static(path.join(__dirname, 'public')));
-  
+
   // Loads all models from the models directory
-  glob.sync('./models/**/*.js').forEach(function(file) {
+  glob.sync('./models/**/*.js').forEach(function (file) {
     require(path.resolve(file)).connectDatabase(conn);
   });
-  
+
   // Loads all routes from the routes directory
   glob.sync('./routes/**/*.js').forEach(function (file) {
     let route = file.slice(6, -3).replaceAll('\\', '/');
@@ -39,19 +40,19 @@ async function main() {
     }
     app.use(route, require(path.resolve(file)));
   });
-  
+
   // catch 404 and forward to error handler
   app.use(function (req, res, next) {
     next(createError(404));
   });
-  
+
   // error handler
   app.use(function (err, req, res, next) {
     // set locals, only providing error in development
     res.locals.message = err.message;
     res.locals.code = err.status || 500;
     res.locals.error = req.app.get('env') === 'development' ? err : {};
-  
+
     // render the error page
     res.status(err.status || 500);
     res.render('error');
