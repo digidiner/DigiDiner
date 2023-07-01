f21d2a0441351c8d37a6754546ae7bcc9bb3013e
Kael Haney <kaelhaney@gmail.com>
2023-07-01 14:08:58 -0400

    Employee create management endpoint

--- routes/api/management.js
+++ routes/api/management.js
@@ -8,7 +8,21 @@ var employee = require('../../models/employee');
 router.get('/employee/list', utils.asyncHandler(async function(req, res) {
     let employeeList = await employee.listEmployees();
     res.status(200).json({
-        'response': employeeList
+        'list': employeeList
+    });
+}));
+
+/* POST create new employee */
+router.post('/employee/create', utils.asyncHandler(async function(req, res) {
+    let id;
+    let newEmployee;
+    do {
+        id = Math.floor(Math.random() * max);
+        newEmployee = new employee();
+    } while (!(await newEmployee.load()));
+    await newEmployee.save();
+    res.status(201).json({
+        'id': newEmployee.id
     });
 }));
 
