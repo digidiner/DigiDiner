9571b2aaafc0de6833db3064a72291ed56d8fa88
Kael Haney <kaelhaney@gmail.com>
2023-07-02 16:50:30 -0400

    Fix employee signup query parameter processing

--- routes/api/common.js
+++ routes/api/common.js
@@ -9,22 +9,22 @@ var employee = require('../../models/employee');
 
 /* POST new employee signup */
 router.post('/employee/signup', utils.asyncHandler(async function(req, res) {
-    if (!req.id || !req.nameFirst || !req.nameLast) {
+    if (!req.body.id || !req.body.nameFirst || !req.body.nameLast) {
         res.status(400).json({
             'error': "Missing Required Fields"
         });
         return;
     }
-    let newEmployee = new employee(req.id);
+    let newEmployee = new employee(req.body.id);
     if (!(await newEmployee.load()) || newEmployee.nameFirst) {
         res.status(403).json({
             'error': "Invalid Employee ID"
         });
         return;
     }
-    newEmployee.passHash = req.pass ? await bcrypt.hash(req.pass, saltRounds) : null;
-    newEmployee.nameFirst = req.nameFirst;
-    newEmployee.nameLast = req.nameLast;
+    newEmployee.passHash = req.body.pass ? await bcrypt.hash(req.body.pass, saltRounds) : null;
+    newEmployee.nameFirst = req.body.nameFirst;
+    newEmployee.nameLast = req.body.nameLast;
     await newEmployee.save();
     res.status(201).json({
         'id': newEmployee.id
