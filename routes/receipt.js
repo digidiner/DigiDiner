94a82ccf0e022795533b72a961246a43eefa42e8
Lindsey Jimenez <lindseyjimenez@gmail.com>
2023-07-04 14:52:35 -0400

    updated call.

--- routes/receipt.js
+++ routes/receipt.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const databaseController = require('./databaseController');
+const databaseController = require('databaseController');
 const nodemailer = require('nodemailer');
 const twilio = require('twilio');
 
