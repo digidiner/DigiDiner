c149b1f922e7d164a24949645adba618866f11cc
Lindsey Jimenez <lindseyjimenez@gmail.com>
2023-07-04 16:31:24 -0400

    Update model v. route structure.

---
+++ models/payment.js
@@ -0,0 +1,115 @@
+const nodemailer = require('nodemailer');
+const twilio = require('twilio');
+
+class Payment {
+    static async connectDatabase(conn) {
+        this.conn = conn;
+        const query = `CREATE TABLE IF NOT EXISTS payments (
+            id INT PRIMARY KEY AUTO_INCREMENT,
+            full_name VARCHAR(100) NOT NULL,
+            card_number VARCHAR(16) NOT NULL,
+            cvv VARCHAR(3) NOT NULL,
+            expiration VARCHAR(7) NOT NULL,
+            zip_code VARCHAR(10) NOT NULL
+        )`;
+        const results = await conn.query(query);
+        return results;
+    }
+
+    static async insertPayment(fullName, cardNumber, cvv, expiration, zipCode) {
+        const query = 'INSERT INTO payments (full_name, card_number, cvv, expiration, zip_code) VALUES (?, ?, ?, ?, ?)';
+        const values = [fullName, cardNumber, cvv, expiration, zipCode];
+        return await this.conn.query(query, values);
+    }
+
+    static calculateSubtotal(cart) {
+        let subtotal = 0;
+        cart.forEach((item) => {
+            subtotal += item.quantity * item.price;
+        });
+        return subtotal.toFixed(2);
+    }
+
+    static calculateTaxes(cart) {
+        const subtotal = this.calculateSubtotal(cart);
+        const taxes = subtotal * 0.1; // Assuming tax rate of 10%
+        return taxes.toFixed(2);
+    }
+
+    static calculateTotal(cart, tip) {
+        const subtotal = this.calculateSubtotal(cart);
+        const taxes = this.calculateTaxes(cart);
+        const total = parseFloat(subtotal) + parseFloat(taxes) + parseFloat(tip);
+        return total.toFixed(2);
+    }
+
+    static async processPayment(req, res) {
+        const { fullName, cardNumber, cvv, expiration, zipCode } = req.body;
+
+        try {
+            const results = await this.insertPayment(fullName, cardNumber, cvv, expiration, zipCode);
+
+            // Retrieve the cart data from wherever it is stored
+            const cart = []; // Replace this with the actual code to retrieve the cart data
+            const tip = req.body.tip; // Retrieve the tip amount from the request body
+
+            res.render('bill', {
+                cart,
+                subtotal: this.calculateSubtotal(cart),
+                taxes: this.calculateTaxes(cart),
+                total: this.calculateTotal(cart, tip),
+            });
+        } catch (error) {
+            console.error('Error processing payment:', error);
+            // Handle the error and return an appropriate response
+            res.sendStatus(500);
+        }
+    }
+
+    static maskCardNumber(cardNumber) {
+        const lastFourDigits = cardNumber.slice(-4);
+        return '*'.repeat(cardNumber.length - 4) + lastFourDigits;
+    }
+
+    static sendEmailReceipt(email) {
+        const transporter = nodemailer.createTransport({
+            service: 'YourEmailService',
+            auth: {
+                user: 'YourEmail',
+                pass: 'YourEmailPassword'
+            }
+        });
+
+        const mailOptions = {
+            from: 'YourEmailAddress',
+            to: email,
+            subject: 'Receipt',
+            text: 'Here is your receipt.'
+            // You can add more details to the email content or attach a PDF receipt if desired
+        };
+
+        transporter.sendMail(mailOptions, (error, info) => {
+            if (error) {
+                console.error('Error sending email:', error);
+            } else {
+                console.log('Email sent:', info.response);
+            }
+        });
+    }
+
+    static sendTextReceipt(phoneNumber) {
+        const twilioClient = twilio('YourTwilioAccountSid', 'YourTwilioAuthToken');
+        const message = 'Here is your receipt.';
+
+        twilioClient.messages
+            .create({
+                body: message,
+                from: 'YourTwilioPhoneNumber',
+                to: phoneNumber
+            })
+            .then(message => console.log('Text message sent:', message.sid))
+            .catch(error => console.error('Error sending text message:', error));
+    }
+}
+
+module.exports = Payment;
--- routes/payment.js
+++ routes/payment.js
@@ -1,37 +1,21 @@
 const express = require('express');
 const router = express.Router();
-const databaseController = require('./controllers/databaseController.js');
+const Payment = require('../../models/payment');
 
 router.get('/', (req, res) => {
-    const total = req.query.total; // Retrieve the total cost from the query parameter
+    const total = req.query.total;
     res.render('payment', { total });
 });
 
 router.post('/', (req, res) => {
     const { fullName, cardNumber, cvv, expiration, zipCode } = req.body;
 
-    // Insert the payment details into the database
-    const query = 'INSERT INTO payments (full_name, card_number, cvv, expiration, zip_code) VALUES (?, ?, ?, ?, ?)';
-    const values = [fullName, cardNumber, cvv, expiration, zipCode];
-
-    databaseController.getConnection()
-        .then(connection => {
-            connection.query(query, values)
-                .then(results => {
-                    connection.release(); // Release the connection back to the pool
-                    // Payment successful, redirect to the receipt page
-                    res.redirect('/receipt');
-                })
-                .catch(error => {
-                    connection.release(); // Release the connection back to the pool
-                    console.error('Error processing payment:', error);
-                    // Handle the error and return an appropriate response
-                    res.sendStatus(500);
-                });
+    Payment.insertPayment(fullName, cardNumber, cvv, expiration, zipCode)
+        .then(() => {
+            res.redirect('/receipt');
         })
-        .catch(error => {
-            console.error('Error establishing database connection:', error);
-            // Handle the error and return an appropriate response
+        .catch((error) => {
+            console.error('Error processing payment:', error);
             res.sendStatus(500);
         });
 });
