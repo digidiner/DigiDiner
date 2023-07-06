d36362eb79b252e986fd0afc4949455f2ae1df49
Lindsey Jimenez <lindseyjimenez@gmail.com>
2023-07-05 23:30:06 -0400

    Updated data.

--- routes/floormap.js
+++ routes/floormap.js
@@ -14,7 +14,7 @@ const tables = [
     { id: 9, status: 'dirty' },
 ];
 
-const menuItems = [
+const lunchItems = [
     {
         name: 'Item 1',
         price: 9.99,
@@ -31,8 +31,47 @@ const menuItems = [
             { name: 'Modification 4', price: 2.49 },
             { name: 'Modification 5', price: 0.99 }
         ]
-    }
+    },
 ];
+const dinnerItems = [
+    {
+        name: 'Item 1',
+        price: 9.99,
+        modifications: [
+            { name: 'Modification 1', price: 1.99 },
+            { name: 'Modification 2', price: 2.99 }
+        ]
+    },
+    {
+        name: 'Item 2',
+        price: 12.99,
+        modifications: [
+            { name: 'Modification 3', price: 1.49 },
+            { name: 'Modification 4', price: 2.49 },
+            { name: 'Modification 5', price: 0.99 }
+        ]
+    },
+];
+const drinkItems = [
+    {
+        name: 'Item 1',
+        price: 9.99,
+        modifications: [
+            { name: 'Modification 1', price: 1.99 },
+            { name: 'Modification 2', price: 2.99 }
+        ]
+    },
+    {
+        name: 'Item 2',
+        price: 12.99,
+        modifications: [
+            { name: 'Modification 3', price: 1.49 },
+            { name: 'Modification 4', price: 2.49 },
+            { name: 'Modification 5', price: 0.99 }
+        ]
+    },
+];
+
 
 // GET the floor map
 router.get('/', function (req, res) {
