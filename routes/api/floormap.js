9c96ff24e01e6f12f89c3da495ab71f271615d3c
Lindsey Jimenez <lindseyjimenez@gmail.com>
2023-07-06 13:50:41 -0400

    Created GET, POST, PUT, DELETE route.

--- routes/api/floormap.js
+++ routes/api/floormap.js
@@ -63,8 +63,7 @@ const drinkItems = [
 ];
 
 
-// GET the floor map
-
+// GET all /tables
 router.get('/', async function (req, res) {
     try {
         const tables = await Table.listTables();
@@ -76,5 +75,70 @@ router.get('/', async function (req, res) {
     }
 });
 
+// GET /tables/:id - Get a specific table
+router.get('/:id', async (req, res) => {
+    const tableId = req.params.id;
+    try {
+        const table = await Table.getTable(tableId);
+        if (table) {
+            res.status(200).json(table);
+        } else {
+            res.status(404).json({ message: 'Table not found' });
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+// POST /tables - Add a new table
+router.post('/', async (req, res) => {
+    const { seats, posX, posY, status } = req.body;
+    try {
+        const newTable = await Table.addTable(seats, posX, posY, status);
+        res.status(201).json(newTable);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+// DELETE /tables/:id - Remove a table
+router.delete('/:id', async (req, res) => {
+    const tableId = req.params.id;
+    try {
+        const success = await Table.removeTable(tableId);
+        if (success) {
+            res.status(200).json({ message: 'Table deleted successfully' });
+        } else {
+            res.status(404).json({ message: 'Table not found' });
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+// PUT /tables/:id - Update a table
+router.put('/:id', async (req, res) => {
+    const tableId = req.params.id;
+    const { seats, posX, posY, status } = req.body;
+    try {
+        const table = await Table.getTable(tableId);
+        if (table) {
+            table.seats = seats || table.seats;
+            table.posX = posX || table.posX;
+            table.posY = posY || table.posY;
+            table.status = status || table.status;
+            await table.save();
+            res.status(200).json(table);
+        } else {
+            res.status(404).json({ message: 'Table not found' });
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+    }
+});
 
 module.exports = router;
