6872e2958711538d64275941d7b5ac42e0aeb52a
Lindsey Jimenez <lindseyjimenez@gmail.com>
2023-07-05 22:53:40 -0400

    Updated test data.

--- routes/order.js
+++ routes/order.js
@@ -2,7 +2,6 @@ var express = require('express');
 var router = express.Router();
 
 // Define a variable to store the order data
-var orderData = [];
 var orderData = [
     {
         name: "Burger",
