dafc8d678e81d0c9f894742f33a77b2e6704c084
Lindsey Jimenez <lindseyjimenez@gmail.com>
2023-07-09 16:03:59 -0400

    Updated profile route.

--- routes/profile.js
+++ routes/profile.js
@@ -1,11 +1,18 @@
 var express = require('express');
 var router = express.Router();
-var Employee = require('./api/common');
+var Employee = require('../models/employee');
 
 
-/* GET users listing. */
-router.get('/', function (req, res) {
-  res.status(200).render('profile', { Employee });
+/* GET profile page. */
+router.get('/:id', function (req, res) {
+  var employeeId = req.params.id;
+  // Retrieve the employee data based on the employeeId
+  var employee = Employee.findById(employeeId);
+  if (employee) {
+    res.status(200).render('profile', { employee });
+  } else {
+    res.status(404).send('Employee not found');
+  }
 });
 
 module.exports = router;
