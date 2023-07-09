a4915580ff11c70b577cb7ba2dddcf51304e7fdd
Josh Boustead <josh.boustead@pemdastech.com>
2023-07-09 01:12:00 -0500

    Update the routes/menu.js for the changes to the /views/partials/menu.js

--- routes/menu.js
+++ routes/menu.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-const lunchItems = [
+const menuItems = [
     {
         name: 'Item 1',
         price: 9.99,
@@ -20,48 +20,52 @@ const lunchItems = [
         ]
     },
 ];
-const dinnerItems = [
-    {
-        name: 'Item 1',
-        price: 9.99,
-        modifications: [
-            { name: 'Modification 1', price: 1.99 },
-            { name: 'Modification 2', price: 2.99 }
-        ]
-    },
-    {
-        name: 'Item 2',
-        price: 12.99,
-        modifications: [
-            { name: 'Modification 3', price: 1.49 },
-            { name: 'Modification 4', price: 2.49 },
-            { name: 'Modification 5', price: 0.99 }
-        ]
-    },
-];
-const drinkItems = [
-    {
-        name: 'Item 1',
-        price: 9.99,
-        modifications: [
-            { name: 'Modification 1', price: 1.99 },
-            { name: 'Modification 2', price: 2.99 }
-        ]
-    },
-    {
-        name: 'Item 2',
-        price: 12.99,
-        modifications: [
-            { name: 'Modification 3', price: 1.49 },
-            { name: 'Modification 4', price: 2.49 },
-            { name: 'Modification 5', price: 0.99 }
-        ]
-    },
-];
+// const dinnerItems = [
+//     {
+//         name: 'Item 1',
+//         price: 9.99,
+//         modifications: [
+//             { name: 'Modification 1', price: 1.99 },
+//             { name: 'Modification 2', price: 2.99 }
+//         ]
+//     },
+//     {
+//         name: 'Item 2',
+//         price: 12.99,
+//         modifications: [
+//             { name: 'Modification 3', price: 1.49 },
+//             { name: 'Modification 4', price: 2.49 },
+//             { name: 'Modification 5', price: 0.99 }
+//         ]
+//     },
+// ];
+// const drinkItems = [
+//     {
+//         name: 'Item 1',
+//         price: 9.99,
+//         modifications: [
+//             { name: 'Modification 1', price: 1.99 },
+//             { name: 'Modification 2', price: 2.99 }
+//         ]
+//     },
+//     {
+//         name: 'Item 2',
+//         price: 12.99,
+//         modifications: [
+//             { name: 'Modification 3', price: 1.49 },
+//             { name: 'Modification 4', price: 2.49 },
+//             { name: 'Modification 5', price: 0.99 }
+//         ]
+//     },
+// ];
 
 /* GET menu listing. */
+// router.get('/', function (req, res) {
+//     res.status(200).render('menu', { lunchItems, dinnerItems, drinkItems });
+// });
+
 router.get('/', function (req, res) {
-    res.status(200).render('menu', { lunchItems, dinnerItems, drinkItems });
+    res.status(200).render('menu', { menuItems });
 });
 
 module.exports = router;
