09a9da59e6dbc79d0b19c4cc2a63c35fbe5d3591
Lindsey Jimenez <lindseyjimenez@gmail.com>
2023-07-10 18:42:48 -0400

    Removed useless export.

--- routes/menu.js
+++ routes/menu.js
@@ -65,7 +65,7 @@ var router = express.Router();
 // });
 
 router.get('/', function (req, res) {
-    res.status(200).render('menu', { menuItems });
+    res.status(200).render('menu');
 });
 
 module.exports = router;
