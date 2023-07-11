b9fedff58be401649ae20796b07d4d27a4b9bb1e
Lindsey Jimenez <lindseyjimenez@gmail.com>
2023-07-10 20:24:46 -0400

    Menu test 4.

--- routes/menu.js
+++ routes/menu.js
@@ -5,6 +5,13 @@ const menuData = require('../models/menuData');
 var itemOption = require('../models/menuItemOption');
 var optionData = require('../models/menuOptionData');
 
+var express = require('express');
+var router = express.Router();
+var menuController = require('../controllers/menuController');
+const menuData = require('../models/menuData');
+var itemOption = require('../models/menuItemOption');
+var optionData = require('../models/menuOptionData');
+
 router.get('/', async (req, res) => {
     try {
         const menu = new menuData();
@@ -12,7 +19,7 @@ router.get('/', async (req, res) => {
         const menuOptions = await optionData.getAllMenuOption();
         const menuAssociations = await itemOption.getOptionsForMenuItem(item.id);
 
-        res.status(200).render('menu', { menuItems, menuOptions, menuAssociations });
+        res.status(200).render('menu', { menuItems, menuOptions, menuAssociations, menuController });
     } catch (error) {
         console.error('Error retrieving menu items:', error);
         res.status(500).send('Internal Server Error');
