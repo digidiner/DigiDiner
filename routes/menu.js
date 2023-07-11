b20623492e4d0ff837397bd6c1917ea40663b0fc
Kael Haney <kaelhaney@gmail.com>
2023-07-11 16:52:51 -0400

    Work on order-specific menu

--- routes/menu.js
+++ routes/menu.js
@@ -3,6 +3,7 @@ var router = express.Router();
 var menuController = require('../controllers/menuController');
 var utils = require('../utils');
 const menuData = require('../models/menuData');
+const Order = require('../models/order');
 var itemOption = require('../models/menuItemOption');
 var optionData = require('../models/menuOptionData');
 
@@ -10,6 +11,16 @@ router.get('/', utils.asyncHandler(async function(req, res) {
     const menuItems = await menuData.getAllMenuItems();
 
     res.status(200).render('menu', { menuItems });
+}));
+
+router.get('/:id', utils.asyncHandler(async function(req, res) {
+    const menuItems = await menuData.getAllMenuItems();
+    const order = new Order(req.params.id);
+    if (await order.load()) {
+        res.status(200).render('menu', { menuItems, order });
+    } else {
+        next(); // Let it 404
+    }
 }));
 
 module.exports = router;
