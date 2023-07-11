a3e7257fb248ba07cb8e02f8186e392b5b38c199
Kael Haney <kaelhaney@gmail.com>
2023-07-11 15:00:41 -0400

    Attempt to make order not use dummy data

--- routes/order.js
+++ routes/order.js
@@ -1,26 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
+var utils = require('../utils');
+
+const menuData = require('../models/menuData');
 const Order = require('../models/order');
 
 // Define a variable to store the order data
-var orderData = [
-    {
-        name: "Burger",
-        count: 2,
-        price: 10.99
-    },
-    {
-        name: "Pizza",
-        count: 1,
-        price: 12.99
-    },
-    {
-        name: "Salad",
-        count: 1,
-        price: 8.99
-    }
-];
+var orderData = [];
 
 // POST /queue endpoint to receive the order data and add it to the order queue
 router.post('/queue', function (req, res) {
@@ -29,9 +16,14 @@ router.post('/queue', function (req, res) {
     res.sendStatus(200);
 });
 
-router.get('/', function (req, res) {
-    res.status(200).render('order', { order: orderData }); // Pass the orderData to the order.ejs template
-});
+router.get('/:id', utils.asyncHandler(async function (req, res, next) {
+    const order = new Order(req.params.id);
+    if (await order.load()) {
+        res.status(200).render('order', { orderItems: await order.getItems(), menuItems: Object.fromEntries((await menuData.getAllMenuItems()).map(item => [item.id, item])) }); // Pass the orderData to the order.ejs template
+    } else {
+        next(); // Let it 404
+    }
+}));
 
 // Add a new route to receive the order data and store it in the orderData variable
 router.post('/data', function (req, res) {
