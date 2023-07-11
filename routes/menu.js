5857cb5775b4f7afbe2659737cf28efc4fb8f8fc
Kael Haney <kaelhaney@gmail.com>
2023-07-11 01:47:08 -0400

    Should fix the menu route

--- routes/menu.js
+++ routes/menu.js
@@ -1,15 +1,16 @@
 var express = require('express');
 var router = express.Router();
 var menuController = require('../controllers/menuController');
+var utils = require('../utils');
 const menuData = require('../models/menuData');
 var itemOption = require('../models/menuItemOption');
 var optionData = require('../models/menuOptionData');
 
-router.get('/', async (req, res) => {
+router.get('/', utils.asyncHandler(async function(req, res) {
     const menuItems = await menuData.getAllMenuItems();
     for (const menuItem of menuItems) menuItem.options = await itemOption.getOptionsForMenuItem(menuItem.id);
 
-    res.status(200).render('menu', { menuItems, menuOptions });
-});
+    res.status(200).render('menu', { menuItems });
+}));
 
 module.exports = router;
