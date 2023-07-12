6d659c418dff33dbd0aed777ccd70f61851ff120
Kael Haney <kaelhaney@gmail.com>
2023-07-12 12:56:50 -0400

    Remove unnecessary stuff from index page

--- routes/index.js
+++ routes/index.js
@@ -4,17 +4,7 @@ var Employee = require('../models/employee')
 const databaseController = require('../controllers/databaseController.js');
 
 /* GET home page. */
-router.get('/', async function (req, res, next) {
-  try {
-    if (!databaseController.dbConnection) {
-      databaseController.dbConnection = await databaseController.getConnection();
-    }
-    await databaseController.dbConnection.ping();
-    res.locals.dbStatus = 'Connected';
-  } catch (err) {
-    res.locals.dbStatus = 'Disconnected';
-    res.locals.error = err;
-  }
+router.get('/', async function (req, res) {
   res.render('index', { Employee });
 });
 
