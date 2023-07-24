7b698e1a105038027538bfed4f7158b04a70f3e8
Lindsey Jimenez <lindseyjimenez@gmail.com>
2023-07-24 13:55:15 -0400

    Updated header.

--- routes/queue.js
+++ routes/queue.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var router = express.Router();
-const fetch = require('node-fetch');
 var Order = require('../models/order');
 
 // Used to verify user is signed in
