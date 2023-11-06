55722090ef7d874d95b3122ed6ffdfbd518efcea
Lindsey Jimenez <lindseyjimenez@gmail.com>
2023-11-06 15:33:04 -0500

    file rename

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ const databaseController = require('../controllers/databaseController.js');
 
 /* GET home page. */
 router.get('/', async function (req, res) {
-  res.render('index', { Employee });
+  res.render('main', { Employee });
 });
 
 /* GET home page. */
